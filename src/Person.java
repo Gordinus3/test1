58522877ec0ac8deeb17b10801bd17b58c37bbcc
GIO LEONARD BACULPO VASQUEZ <2020846@cc.usls.edu>
2023-05-11 12:19:57 +0800

    first commit

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    String FirstName, LastName;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
